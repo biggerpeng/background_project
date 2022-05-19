98622a63aa50c84a75e529a3bb170538eb60bbe0
typeng <typeng@live.cn>
2022-05-19 22:48:28 +0800

    实现平台属性管理静态页面

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -4,24 +4,43 @@
       <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
     </el-card>
     <el-card>
-      <el-table :data="attrList" style="width: 100%" border>
-        <el-table-column type="index" label="序号" width="100px" align="center"> </el-table-column>
-        <el-table-column prop="attrName" label="属性名称" width="150px"> </el-table-column>
-        <el-table-column label="属性值列表" width="width">
-          <!-- 注意elementUi插槽prop传过来的为row\$index -->
-          <template v-slot="{ row }">
-            <el-tag type="success" v-for="attrValue in row.attrValueList" :key="attrValue.id" style="margin: 0 10px">{{
-              attrValue.valueName
-            }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="prop" label="操作" width="200px">
-          <template v-solt="{ row }">
-            <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="isShowTable">
+        <el-button type="primary" icon="el-icon-plus" :disabled="!categoryId.category3Id" @click="addOrChangeAttr"> 添加</el-button>
+        <el-table :data="attrList" style="width: 100%" border>
+          <el-table-column type="index" label="序号" width="100px" align="center"> </el-table-column>
+          <el-table-column prop="attrName" label="属性名称" width="150px"> </el-table-column>
+          <el-table-column label="属性值列表" width="width">
+            <!-- 注意elementUi插槽prop传过来的为row\$index -->
+            <template v-slot="{ row }">
+              <el-tag type="success" v-for="attrValue in row.attrValueList" :key="attrValue.id" style="margin: 0 10px">{{
+                attrValue.valueName
+              }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="200px">
+            <template v-solt="{ row }">
+              <el-button type="warning" icon="el-icon-edit" size="mini" @click="addOrChangeAttr">修改</el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div v-show="!isShowTable">
+        <el-form :inline="true" ref="form" label-width="80px">
+          <el-form-item label="属性名">
+            <el-input placeholder="请输入属性名"></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button @click="cancel">取消</el-button>
+        <el-table style="width: 100%" border>
+          <el-table-column type="index" label="序号" width="100px" align="center"> </el-table-column>
+          <el-table-column prop="prop" label="属性值名称" width="width"> </el-table-column>
+          <el-table-column prop="prop" label="操作" width="width"> </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="cancel">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -32,7 +51,8 @@
     data() {
       return {
         categoryId: {},
-        attrList: []
+        attrList: [],
+        isShowTable: true
       }
     },
     methods: {
@@ -47,6 +67,14 @@
         if (result.code === 200) {
           this.attrList = result.data
         }
+      },
+      // 添加或修改平台属性
+      addOrChangeAttr() {
+        this.isShowTable = false
+      },
+      // 取消添加或修改
+      cancel() {
+        this.isShowTable = true
       }
     }
   }
